783202089b9e0faf76043ba35f468a55f62938af
Lucas Santos <43353219+iFallenHunt@users.noreply.github.com>
2023-10-18 15:52:26 -0300

    Backend PicPay job project

---
+++ src/test/java/com/picpayproject/PicpayprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpayproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
