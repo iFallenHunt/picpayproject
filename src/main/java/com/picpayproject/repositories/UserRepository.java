fd353d4f4c6b6b6768776b1d86989286f3a3bbf5
Lucas Santos <43353219+iFallenHunt@users.noreply.github.com>
2023-10-18 17:57:27 -0300

    Backend PicPay job project

---
+++ src/main/java/com/picpayproject/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.picpayproject.repositories;
+
+import com.picpayproject.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
