411c8d4a9d69d0e0f50f9ed86d7845e3ed062815
Lucas Santos <43353219+iFallenHunt@users.noreply.github.com>
2023-10-23 14:54:06 -0300

    feat: create DTO's

---
+++ src/main/java/com/picpayproject/dtos/NotificationDTO.java
@@ -0,0 +1,4 @@
+package com.picpayproject.dtos;
+
+public record NotificationDTO(String email, String message) {
+}
---
+++ src/main/java/com/picpayproject/dtos/UserDTO.java
@@ -0,0 +1,10 @@
+package com.picpayproject.dtos;
+
+import java.math.BigDecimal;
+
+import com.picpayproject.domain.user.UserType;
+
+public record UserDTO(String firstName, String lastName, String document, BigDecimal balance, String email,
+                String password, UserType userType) {
+
+}
