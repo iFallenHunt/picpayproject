636bb2421caa26782cc3285227f16924fdf9b03b
Lucas Santos <43353219+iFallenHunt@users.noreply.github.com>
2023-10-23 14:53:42 -0300

    feat: create Exceptions

---
+++ src/main/java/com/picpayproject/dtos/ExceptionDTO.java
@@ -0,0 +1,4 @@
+package com.picpayproject.dtos;
+
+public record ExceptionDTO(String message, String statusCode) {
+}
---
+++ src/main/java/com/picpayproject/infra/ControllerExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.picpayproject.infra;
+
+import com.picpayproject.dtos.ExceptionDTO;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity threatDuplicatEntry(DataIntegrityViolationException exception) {
+        ExceptionDTO exceptionDTIO = new ExceptionDTO("Usuario ja cadastrado" + "\nUser already registered", "400");
+        return ResponseEntity.badRequest().body(exceptionDTIO);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404(EntityNotFoundException exception) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity threatGeneralException(Exception exception) {
+        ExceptionDTO exceptionDTO = new ExceptionDTO(exception.getMessage(), "500");
+        return ResponseEntity.internalServerError().body(exceptionDTO);
+    }
+
+}
